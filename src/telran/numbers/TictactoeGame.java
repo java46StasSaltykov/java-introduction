6b1652ad2e51c15d9d5eacafc194350cd35641ab
StasSaltykov <stasyanych92@gmail.com>
2022-06-30 18:26:32 +0300

    hw#5 fix

--- src/telran/numbers/TictactoeGame.java
+++ src/telran/numbers/TictactoeGame.java
@@ -1,90 +1,92 @@
 package telran.numbers;
 
-import java.util.Arrays;
-
 public class TictactoeGame {
 	/**
 	 * 
-	 * @param matrix square matrix (nRows == nColumns); where nRows >= 3
-	 * @param nRow (number of row)
-	 * @param nColumn (number of column)
-	 * @param symb (being filled symbol either 'x' or '0')
-	 * @return 0 - game isn't over; 1 - game is over with the winner's move; 2 - game is over with draw
+	 * @param matrix square matrix (nRows == nColumns; where nRows >= 3)
+	 * @param nRow   (number of row)
+	 * @param nCol   (number of column)
+	 * @param symb   (being filled symbol either 'x' or '0')
+	 * @return 0 - game isn't over; 1 - game is over with the winner's move; 2 -
+	 *         game is over with draw
 	 */
-	public static int tictactoeMove(char matrix[][], int nRow, int nColumn, char symb) {
-		char[] newRow = insertNumber(matrix[nRow], nColumn, symb);
-		char[][] newMat = removeRow(matrix, nRow);
-		char[][] newMat1 = insertRow(newMat, newRow, nRow);
-		
-		for(int i = 0; i < newMat1.length; i++) {
-			int count = 0;
-			for(int j = 0; j < newMat1[i].length; j++) {
-				if(newMat1[i][j] == symb) {
-					count++;
-					if (count == 3) {
-						return 1;
-					}
+	public static int tictactoeMove(char matrix[][], int nRow, int nCol, char symb) {
+		matrix[nRow][nCol] = symb;
+		if (isFullRow(matrix, nRow, symb) || isFullColumn(matrix, nCol, symb))
+			return 1;
+		if (isLeftDiagonal(matrix, nRow, nCol, symb) || isRightDiagonal(matrix, nRow, nCol, symb))
+			return 1;
+		if (isDraw(matrix))
+			return 2;
+		return 0;
+	}
+
+	private static boolean isDraw(char[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j] != 'x' && matrix[i][j] != '0') {
+					return false;
 				}
 			}
 		}
-		for(int i = 0; i < newMat1.length; i++) {
-			int count = 0;
-			for(int j = 0; j < newMat1[i].length; j++) {
-				if(newMat1[j][i] == symb) {
-					count++;
-					if (count == 3) {
-						return 1;
-					}
+		return true;
+	}
+
+	private static boolean isRightDiagonal(char[][] matrix, int row, int column, char symb) {
+		if (isOnRightDiagonal(row, column, matrix.length - 1)) {
+			for (int i = 0, j = matrix.length - 1; i < matrix.length; i++, j--) {
+				if (matrix[i][j] != symb) {
+					return false;
 				}
 			}
+			return true;
+
 		}
-		for(int i = 0; i < newMat1.length; i++) {
-			int count = 0;
-			for(int j = 0; j < newMat1[i].length; j++) {
-				if(newMat1[j][j] == symb) {
-					count++;
-					if (count == 3) {
-						return 1;
-					}
+		return false;
+	}
+
+	private static boolean isOnRightDiagonal(int row, int column, int maxIndex) {
+		return row + column == maxIndex;
+	}
+
+	private static boolean isLeftDiagonal(char[][] matrix, int row, int column, char symb) {
+		if (isOnLeftDiagonal(row, column)) {
+			for (int i = 0, j = 0; i < matrix.length; i++, j++) {
+				if (matrix[i][j] != symb) {
+					return false;
 				}
 			}
+			return true;
+
 		}
-		for(int i = 0; i < newMat1.length; i++) {
-			int count = 0;
-			for(int j = 0; j < newMat1[i].length; j++) {
-				if(newMat1[i][newMat1[i].length - 1 - i] == symb) {
-					count++;
-					if (count == 3) {
-						return 1;
-					}
-				}
+		return false;
+
+	}
+
+	private static boolean isOnLeftDiagonal(int row, int column) {
+		return row == column;
+	}
+
+	private static boolean isFullColumn(char[][] matrix, int column, char symb) {
+		boolean res = true;
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][column] != symb) {
+				res = false;
+				break;
 			}
 		}
-		return 2;
-	}	
-	
-	
-	static char[] insertNumber(char ar[], int index, char symb) {
-		char res[] = new char[ar.length + 1];
-		System.arraycopy(ar, 0, res, 0, index);
-		res[index] = symb;
-		System.arraycopy(ar, index, res, index + 1, ar.length - index);
 		return res;
 	}
-	static char[][] removeRow(char matrix[][], int index) {
-		char res[][] = new char[matrix.length - 1][];
-		System.arraycopy(matrix, 0, res, 0, index);
-		System.arraycopy(matrix, index + 1, res, index, res.length - index);
-		return res;
-	}
-	static char[][] insertRow(char matrix[][], char row[], int index) {
-		char res[][] = new char[matrix.length + 1][];
-		if (row.length != matrix[0].length) {
-			row = Arrays.copyOf(row, matrix[0].length);
+
+	private static boolean isFullRow(char[][] matrix, int row, char symb) {
+		boolean res = true;
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[row][j] != symb) {
+				res = false;
+				break;
+			}
 		}
-		System.arraycopy(matrix, 0, res, 0, index);
-		res[index] = row;
-		System.arraycopy(matrix, index, res, index + 1, matrix.length - index);
 		return res;
 	}
+
 }
--- src/telran/numbers/tests/TictactoeTest.java
+++ src/telran/numbers/tests/TictactoeTest.java
@@ -1,7 +1,6 @@
 package telran.numbers.tests;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,40 +10,39 @@ class TictactoeTest {
 
 	@Test
 	void testTictactoeMove() {
-		//TODO
-		char matrix1[][] = {
-				{'0', 'x', 'x'},
-				{'x', '0', 'x'},
-				{'0', '0'}
-		};
-		char matrix2[][] = {
-				{'0', 'x', 'x'},
-				{'x', '0', 'x'},
-				{'x', '0'}
-		};
-		char matrix3[][] = {
-				{'0', 'x', 'x'},
-				{'x', 'x', '0'},
-				{'0', '0'}
-		};
-		char matrix4[][] = {
-				{'0', 'x'},
-				{'x', '0'},
-				{'0', '0'}
-		};
-		char matrix5[][] = {
-				{'0', 'x', 'x'},
-				{'x', 'x', '0'},
-				{'0', '0'}
-		};
-		
-		
-		assertEquals(1, TictactoeGame.tictactoeMove(matrix1, 2, 2, '0'));
-		assertEquals(1, TictactoeGame.tictactoeMove(matrix1, 2, 2, 'x'));
-		assertEquals(1, TictactoeGame.tictactoeMove(matrix2, 2, 2, '0'));
-		assertEquals(1, TictactoeGame.tictactoeMove(matrix3, 2, 0, 'x'));
-//		assertEquals(0, TictactoeGame.tictactoeMove(matrix4, 1, 2, 'x'));
-//		assertEquals(2, TictactoeGame.tictactoeMove(matrix5, 2, 2, 'x'));
+	char matrixWinnerColumnX[][] = {
+			{'x', '0', 'x'},
+			{'x', '0', '0'},
+			{ 0 ,  0,   0 }
+	};
+	char matrixWinnerLeftDiagonalO[][] = {
+			{'0', 'x', 'x'},
+			{'x', '0', '0'},
+			{'x' ,'x',  0 }
+	};
+	char matrixWinnerRightDiagonalX[][] = {
+			{'0', '0', 'x'},
+			{'x',  0,  '0'},
+			{'x' ,'x',  0 }
+	};
+	char matrixWinnerRowO[][] = {
+			{'x', '0', 'x'},
+			{'0',  0,  '0'},
+			{'x' ,'x',  0  }
+	};
+	char matrixDraw[][] = {
+			{'0', '0', 'x'},
+			{'x', 'x', '0'},
+			{'0' ,'x',  0 }
+	};
+	char emptyMatrix[][] = new char[3][3];
+	assertEquals(1, TictactoeGame.tictactoeMove(matrixWinnerColumnX, 2, 0, 'x'));
+	assertEquals(1, TictactoeGame.tictactoeMove(matrixWinnerLeftDiagonalO, 2, 2, '0'));
+	assertEquals(1, TictactoeGame.tictactoeMove(matrixWinnerRightDiagonalX, 1, 1, 'x'));
+	assertEquals(1, TictactoeGame.tictactoeMove(matrixWinnerRowO, 1, 1, '0'));
+	assertEquals(2, TictactoeGame.tictactoeMove(matrixDraw, 2, 2, 'x'));
+	assertEquals(0, TictactoeGame.tictactoeMove(emptyMatrix, 1, 2, 'x'));
+	
 	}
 
 }
